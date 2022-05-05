2b099d74a6be0b312c267a67f90da199653baf4f
snaulX <alexandergunger666@gmail.com>
2022-05-05 16:11:34 +0600

    Remove old project files

--- src/Tangara.h
+++
@@ -1,7 +0,0 @@
-#ifndef TANGARA_TANGARA_H
-#define TANGARA_TANGARA_H
-
-#include "TangaraCore.h"
-#include "File.h"
-
-#endif //TANGARA_TANGARA_H
--- src/test/std.c
+++
@@ -1,9 +0,0 @@
-#include "std.h"
-
-void init_std()
-{
-    TgClass str = tgInitClass(TG_VISIBILITY_PUBLIC, TG_MEMBTYPE_DEFAULT, STR("string"));
-    strClass = &str;
-    TgClass _int = tgInitClass(TG_VISIBILITY_PUBLIC, TG_MEMBTYPE_DEFAULT, STR("int"));
-    intClass = &_int;
-}
